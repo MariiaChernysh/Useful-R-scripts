4e4bd71092d709109baaaf17278cf4c9a2154221
Mariia Chernysh <m.chernysh93@gmail.com>
2020-11-20 15:44:01 +0200

    Update DailyTable.R

--- DailyTable.R
+++ DailyTable.R
@@ -1,7 +1,6 @@
 # This script provides .csv output containing a non-formatted pivot table with several groupings
 # The output will present a summary on Managers' KPIs divided by traffic sources they use.
-# The tricky part is that different Managers can work with the same Traffic Source and we should calculate 
-# all data considering this issue.
+# The tricky part is that different Managers can work with the same Traffic Sources and their stats should be calculated considering this issue.
 library(stringr)
 library(dplyr)
 library(devtools)
